93a4f88fbf824ede85d67cfcb7bad892a3f07e11
Devin Davis <devinrdavis555@gmail.com>
2020-07-31 21:02:06 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,13 +1,12 @@
 require 'pry'
 
 def generate_star_date
-  binding.pry
   (rand(100000) + 400000) / 10.0
 end
 
 
 def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+  puts "Captain's Log, star date #{star_date}."
 end
 
 def engage
