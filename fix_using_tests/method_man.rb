e75ad7bce261f8ebd40c90d0b3f2f94b2efc051e
Devin Davis <devinrdavis555@gmail.com>
2020-07-31 20:40:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,8 @@
-require 'pry'
+
 def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
 end
 
-def play_game(player1)
-  binding.pry
+def play_game(player1, player2)
   "#{player1} is better than #{player2}"
 end
